99ca4cb5ee06bdce6e18534943e1771d3dfce502
Alex <me@alexb.ninja>
2014-10-30 13:28:28 -0700

    initial commit

--- app.rb
+++ app.rb
@@ -1,5 +1,6 @@
 require 'guillotine'
 require 'redis'
+require 'Haml'
 
 module Katana
     class App < Guillotine::App
@@ -10,15 +11,8 @@ module Katana
       set :service => Guillotine::Service.new(adapter, :strip_query => false,
                                               :strip_anchor => false)
 
-      # authenticate everything except GETs
-      before do
-        unless request.request_method == "GET"
-          protected!
-        end
-      end
-
       get '/' do
-        "Shorten all the URLs"
+        haml :index
       end
 
       if ENV['TWEETBOT_API']
@@ -33,32 +27,5 @@ module Katana
           end
         end
       end
-
-      # helper methods
-      helpers do
-
-        # Private: helper method to protect URLs with Rack Basic Auth
-        #
-        # Throws 401 if authorization fails
-        def protected!
-          return unless ENV["HTTP_USER"]
-          unless authorized?
-            response['WWW-Authenticate'] = %(Basic realm="Restricted Area")
-            throw(:halt, [401, "Not authorized\n"])
-          end
-        end
-
-        # Private: helper method to check if authorization parameters match the
-        # set environment variables
-        #
-        # Returns true or false
-        def authorized?
-          @auth ||=  Rack::Auth::Basic::Request.new(request.env)
-          user = ENV["HTTP_USER"]
-          pass = ENV["HTTP_PASS"]
-          @auth.provided? && @auth.basic? && @auth.credentials && @auth.credentials == [user, pass]
-        end
-      end
-
     end
 end
