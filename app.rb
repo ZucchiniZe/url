36e146b83ab7d4b1bccc5cb5089651b6caaa7d9e
Alex <me@alexb.ninja>
2014-10-30 15:14:48 -0700

    update deprecated guillotine

--- app.rb
+++ app.rb
@@ -7,7 +7,7 @@ module Katana
       # use redis adapter with redistogo
       uri = URI.parse(ENV["REDISCLOUD_URL"])
       REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
-      adapter = Guillotine::Adapters::RedisAdapter.new REDIS
+      adapter = Guillotine::RedisAdapter.new REDIS
       set :service => Guillotine::Service.new(adapter, :strip_query => false,
                                               :strip_anchor => false)
 
